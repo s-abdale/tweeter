6a8778e757ff102a021aada5f583726e84ed01c6
Safia Abdale <safia.abdale@gmail.com>
2021-12-16 14:17:06 +0000

    Counter changes colour and becomes negative after reaching 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,26 +2,18 @@
 
 $(document).ready(function() {
   // --- our code goes here ---
-  console.log(`here I am`)
+  console.log(`Character counter loaded`)
 
   $(`#tweetText`).on('keyup', function() {
-    // alert(`Text box clicked ✅`)
     let tweetLength = $(this).val().length;
-    // let counter = $(`#counter`).val()
+    let counter = $(`#counter`);
 
     if (tweetLength <= 140) {
-      // console.log(`Tweet length: ${tweetLength}`);
-      // console.log($(`#counter`).val()-tweetLength);
-      $(`#counter`).text(140-tweetLength)
+      counter.text(140-tweetLength)
     } else {
       console.log(`TOO LONG`);
-      $(`#counter`).text(0+(tweetLength-140))
+      counter.text(`-${0+(tweetLength-140)}`).css("color", "red");
+      // NOTE: functional, but move colour change to CSS
     }
   })
 });
-
-
-// document.addEventListener("click", () => {
-//   console.log("You just clicked somewhere on this page.");
-//   alert(`hi!👋🏽`)
-// });
